da8e1cec903d8a48dac95ec339c2db14ae22956f
farok-amo <farok.amo@gmail.com>
2023-12-26 23:23:15 +0000

    updated req and added data

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from logger import logging
 def error_message_detail(error, error_detail:sys):
 
     _,_,exc_tb=error_detail.exc_info()
@@ -18,3 +18,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
